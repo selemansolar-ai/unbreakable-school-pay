7ce84e66a1d152e4b9c8663b5ecb65f4cccd3618
selemansolar-ai <magangamichaelseleman@gmail.com>
2026-08-17 13:41:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -103,8 +103,8 @@ elif st.session_state.type == "Mkuu":
 		st.metric("Jumla ya Madeni", f"Tsh {df['deni'].sum():,}")
 		st.dataframe(df, use_container_width=True)
 
-   with tab2:
-	   st.header("Ongeza Mzazi Mpya")
+    with tab2:
+		st.header("Ongeza Mzazi Mpya")
 		namba_mpya = st.text_input("Namba ya Mzazi +255..")
 		mtoto_namba = st.selectbox("Muunganishe na Mwanafunzi"), df['namba'])
 		if st.button("Ongeza Mzazi"):
